867f22ec08ac9be9d8d2a3aad606875c31439c61
Priyanshu Pandey <priyanshupandey25012003@gmail.com>
2024-09-04 13:41:44 +0530

    cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Comment from './components/comments'
 function App() {
   return (
